60e9f039601c8bd66d5564139b5cec2d8d761f33
fam4r <fabrizio.margotta@tuta.io>
2019-03-04 18:47:13 +0100

    autoenv - also add serial file

--- .autoenv.zsh
+++ .autoenv.zsh
@@ -1,9 +1,9 @@
 function sendomp() {
-    scp {Makefile,omp-earthquake.c,hpc.h} uniomp:/home/local/STUDENTI/fabrizio.margotta/
+    scp {Makefile,earthquake.c,omp-earthquake.c,hpc.h} uniomp:/home/local/STUDENTI/fabrizio.margotta/
 }
 
 function sendcuda() {
-    scp {Makefile,cuda-earthquake.cu,hpc.h} unicuda:/home/local/STUDENTI/fabrizio.margotta/
+    scp {Makefile,earthquake.c,cuda-earthquake.cu,hpc.h} unicuda:/home/local/STUDENTI/fabrizio.margotta/
 }
 
 function recvres() {
