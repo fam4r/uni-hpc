7f27e465f8b3515286e7a0c318f0d8da3c29919b
fam4r <fabrizio.margotta@tuta.io>
2019-09-15 19:34:19 +0200

    perf eval utility: style check

--- project/src/omp-speedup.py
+++ project/src/omp-speedup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import math
 import os
 import csv
 import logging
@@ -8,11 +7,11 @@ logging.basicConfig(level=logging.INFO)
 
 exe_name = "omp-earthquake"
 steps = 100000
-#p_size = [256, 512, 1024]
+# p_size = [256, 512, 1024]
 p_size = [256]
 
 # [1, 2, 4, 8, 16, 32, 64] threads used
-#n_threads = list(2**x for x in range(0,6))
+# n_threads = list(2**x for x in range(0,6))
 n_threads = [1, 8]
 
 # run the simulation N times and take the average execution time
@@ -27,6 +26,7 @@ strong_filename = "graphs/data/omp-strong-py.dat"
 
 weak_scaling = dict()
 
+
 def compute_weak_scaling():
     for i in range(1, n_threads[len(n_threads) - 1] + 1):
         avg_exe_time = 0
@@ -34,50 +34,53 @@ def compute_weak_scaling():
             out_file = "/dev/null"
             res_file = "res_weak{}_{}".format(i, k)
             # e(l($N0 * $N0 * $N0 * $p)/3)
-            p_size_weak = round((weak_psize**3) * i)**(1./3.), 4)
+            p_size_weak = round(((weak_psize**3) * i)**(1. / 3.), 4)
             logging.info("run #{}: L{}, {} thread".format(k, p_size_weak, i))
             res_file = "res_weak{}_{}".format(i, k)
-            os.system("OMP_NUM_THREADS={} ./{} {} {} > {} 2>{}"
-                .format(i, exe_name, steps, p_size_weak, out_file, res_file))
+            os.system("OMP_NUM_THREADS={} ./{} {} {} > {} 2>{}".format(
+                i, exe_name, steps, p_size_weak, out_file, res_file))
             with open(res_file) as f:
                 for l in f:
                     if "parallel" in l:
                         exe_time = float(l.split(':')[1].split(' ')[1])
                         logging.info("exe_time #{} = {}".format(k, exe_time))
                         avg_exe_time += exe_time
-                        logging.info("avg_exe_time #{} = {}".format(k, avg_exe_time))
+                        logging.info("avg_exe_time #{} = {}".format(
+                            k, avg_exe_time))
         if i == 1:
             t_serial = avg_exe_time
             logging.info("t_serial = {}".format(t_serial))
-        weak_scaling[i] = round(float(t_serial / avg_exe_time),4)
+        weak_scaling[i] = round(float(t_serial / avg_exe_time), 4)
         logging.info("weak_scaling = {}".format(weak_scaling))
+
 
 compute_weak_scaling()
 
 t_serial = dict()
 speedup = dict()
 strong_scaling = dict()
-for i in n_threads: # for each number of threads
+for i in n_threads:  # for each number of threads
     speedup[i] = dict()
     strong_scaling[i] = dict()
 
 # run speedup / strong
-for i in n_threads: # for each number of threads
-    for j in p_size: # for each problem size
+for i in n_threads:  # for each number of threads
+    for j in p_size:  # for each problem size
         avg_exe_time = 0
         for k in range(1, n_run + 1):
             logging.info("run #{}: L{}, {} thread".format(k, j, i))
             out_file = "/dev/null"
             res_file = "res{}_{}_{}".format(j, i, k)
-            os.system("OMP_NUM_THREADS={} ./{} {} {} > {} 2>{}"
-                .format(i, exe_name, steps, j, out_file, res_file))
+            os.system("OMP_NUM_THREADS={} ./{} {} {} > {} 2>{}".format(
+                i, exe_name, steps, j, out_file, res_file))
             with open(res_file) as f:
                 for l in f:
                     if "parallel" in l:
                         exe_time = float(l.split(':')[1].split(' ')[1])
                         logging.info("exe_time #{} = {}".format(k, exe_time))
                         avg_exe_time += exe_time
-                        logging.info("avg_exe_time #{} = {}".format(k, avg_exe_time))
+                        logging.info("avg_exe_time #{} = {}".format(
+                            k, avg_exe_time))
         avg_exe_time /= n_run
         logging.info("avg_exe_time = {}".format(avg_exe_time))
         if i == 1:
@@ -88,8 +91,9 @@ for i in n_threads: # for each number of threads
         strong_scaling[i][j] = round(float(speedup[i][j] / i), 4)
         logging.info("strong_scaling = {}".format(strong_scaling))
 
-with open(speedup_filename, mode='w') as speedup_file, open(strong_filename,
-        mode='w') as strong_file:
+with open(speedup_filename,
+          mode='w') as speedup_file, open(strong_filename,
+                                          mode='w') as strong_file:
     # speedup
     speedup_fieldnames = ['THREAD']
     speedup_fieldnames += p_size
